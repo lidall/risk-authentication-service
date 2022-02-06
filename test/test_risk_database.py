1d1c8ebbc3d1f6346f27951bbcdd7c0a0f29496e
Lida Liu <lindaliu0712@gmail.com>
2022-02-06 16:20:18 +0100

    Correct errors; Update tox file

--- test/test_risk_database.py
+++ test/test_risk_database.py
@@ -19,32 +19,32 @@ class TestRiskDatabase(unittest.TestCase):
         self.assertEqual(self.db.knownUserList, ['Bob', 'Kevin'])
 
     def test_set_knownIPList(self):
-        self.db.set_knownUserList('10.192.2.2')
+        self.db.set_knownIPList('10.192.2.2')
         self.assertEqual(self.db.knownIPList, ['10.192.2.2'])
-        self.db.set_knownUserList('10.192.10.22')
+        self.db.set_knownIPList('10.192.10.22')
         self.assertEqual(self.db.knownIPList, ['10.192.2.2', '10.192.10.22'])
 
     def test_set_successfulLoginDict(self):
         # Test if old Bob record has been ignored
         self.db.set_successfulLoginDict('Bob', 1644151000)
-        self.assertEqual(self.db.set_successfulLoginDict['Bob'], 1644151100)
+        self.assertEqual(self.db.successfulLoginDict['Bob'], 1644151100)
         # Test if new Bob record has been updated
         self.db.set_successfulLoginDict('Bob', 1644151500)
-        self.assertEqual(self.db.set_successfulLoginDict['Bob'], 1644151500)
+        self.assertEqual(self.db.successfulLoginDict['Bob'], 1644151500)
         # Test if new Kevin record has been added into the dict
         self.db.set_successfulLoginDict('Kevin', 1644151530)
-        self.assertEqual(self.db.set_successfulLoginDict['Kevin'], 1644151530)
+        self.assertEqual(self.db.successfulLoginDict['Kevin'], 1644151530)
 
     def test_set_failedLoginDict(self):
         # Test if old Bob record has been ignored
         self.db.set_failedLoginDict('Bob', 1644151009)
-        self.assertEqual(self.db.set_successfulLoginDict['Bob'], 1644151199)
+        self.assertEqual(self.db.successfulLoginDict['Bob'], 1644151199)
         # Test if new Bob record has been updated
         self.db.set_failedLoginDict('Bob', 1644151599)
-        self.assertEqual(self.db.set_successfulLoginDict['Bob'], 1644151599)
+        self.assertEqual(self.db.successfulLoginDict['Bob'], 1644151599)
         # Test if new Kevin record has been added into the dict
         self.db.set_failedLoginDict('Kevin', 1644151530)
-        self.assertEqual(self.db.set_successfulLoginDict['Kevin'], 1644151530)
+        self.assertEqual(self.db.successfulLoginDict['Kevin'], 1644151530)
 
     def set_failedLoginCount(self):
         # Since in the main function I have simplify the adding method:
